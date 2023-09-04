abe0cb7b2b890b30af4941b781bcd1791fbecf64
kurogane677 <85788933+kurogane677@users.noreply.github.com>
2023-09-04 00:25:54 -0700

    Update ApproverAction.java

--- src/main/java/com/test/plugin/ApproverAction.java
+++ src/main/java/com/test/plugin/ApproverAction.java
@@ -2,9 +2,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package id.co.itasoft.hrdc.plugin;
+package com.test.plugin;
 
-import id.co.itasoft.hrdc.plugin.dao.ApproverActionDao;
+import com.test.plugin.dao.ApproverActionDao;
 import java.util.HashMap;
 import java.util.Map;
 import org.joget.apps.app.model.AppDefinition;
