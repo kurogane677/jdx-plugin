5b819aa29b156a9eb1dfb2530538896b8524d19f
Yusril <muhammadyusril365@gmail.com>
2023-09-04 14:23:47 +0700

    Add Multi Approve button

---
+++ src/main/java/com/test/plugin/ApproverAction.java
@@ -0,0 +1,197 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package id.co.itasoft.hrdc.plugin;
+
+import id.co.itasoft.hrdc.plugin.dao.ApproverActionDao;
+import java.util.HashMap;
+import java.util.Map;
+import org.joget.apps.app.model.AppDefinition;
+import org.joget.apps.app.service.AppUtil;
+import org.joget.apps.datalist.model.DataList;
+import org.joget.apps.datalist.model.DataListActionDefault;
+import org.joget.apps.datalist.model.DataListActionResult;
+import org.joget.apps.form.model.FormData;
+import org.joget.commons.util.LogUtil;
+import org.joget.workflow.model.service.WorkflowManager;
+
+/**
+ *
+ * @author WEEBS
+ */
+public class ApproverAction extends DataListActionDefault{
+    
+    String pluginName = "HRDC - Approver Layer 4";
+    ApproverActionDao aad = new ApproverActionDao();
+
+    @Override
+    public String getName() {
+        return pluginName;
+    }
+
+    @Override
+    public String getVersion() {
+        return "1.0";
+    }
+
+    @Override
+    public String getDescription() {
+        return pluginName;
+    }
+
+    @Override
+    public String getLinkLabel() {
+        String label = getPropertyString("label");
+        if (label == null || label.isEmpty()) {
+            label = pluginName;
+        }
+        return label;
+    }
+
+    @Override
+    public String getHref() {
+        return getPropertyString("href");
+    }
+
+    @Override
+    public String getTarget() {
+        return "post";
+    }
+
+    @Override
+    public String getHrefParam() {
+        return getPropertyString("hrefParam");
+    }
+
+    @Override
+    public String getHrefColumn() {
+        return getPropertyString("hrefColumn");
+    }
+
+    @Override
+    public String getConfirmation() {
+        String confirm = getPropertyString("confirmation");
+        if (confirm == null || confirm.isEmpty()) {
+            confirm = "Are you sure?";
+        }
+        return confirm;
+    }
+
+    @Override
+    public DataListActionResult executeAction(DataList dl, String[] strings) {
+        DataListActionResult result = new DataListActionResult();
+        LogUtil.info(pluginName, "Masuk Execute");
+        
+        String submodule = getPropertyString("submodule");
+        String submitType = getPropertyString("submittype");
+        String actorUsername = getPropertyString("actor_username");
+        String actorFullname = getPropertyString("actor_fullname");
+        String remarkMessage = getPropertyString("remark_message");
+        String tableProcessName = getPropertyString("tableProcessName");
+        String tableName = getPropertyString("tableNameAudit");
+        String applicationStatus = "";
+        String auditActivity = "";
+        String auditStatus = "";
+        String applicationLastAction = "";
+        
+        if(submitType.equalsIgnoreCase("Approved")){
+            applicationStatus = "APPLICATION APPROVED";
+            auditActivity = "Application Approved";
+            auditStatus = "Approved";
+            applicationLastAction = "Approved";
+        }else if(submitType.equalsIgnoreCase("Rejected")){
+            applicationStatus = "APPLICATION REJECTED";
+            auditActivity = "Application Rejected";
+            auditStatus = "Rejected";
+            applicationLastAction = "Rejected";
+        }else{
+            applicationStatus = "APPLICATION RETURNED";
+            auditActivity = "Application Returned";
+            auditStatus = "Returned";
+            applicationLastAction = "Returned";   
+        }
+        
+        for(String key : strings){
+            LogUtil.info(pluginName, "ID : "+key);
+            LogUtil.info(pluginName, "submodule: " + submodule);
+            LogUtil.info(pluginName, "actorUsername: " + actorUsername);
+            LogUtil.info(pluginName, "actorFullname: " + actorFullname);
+            LogUtil.info(pluginName, "remarkMessage: " + remarkMessage);
+            LogUtil.info(pluginName, "Action : " + submitType);
+            LogUtil.info(pluginName, "applicationStatus: " + applicationStatus);
+            LogUtil.info(pluginName, "auditActivity: " + auditActivity);
+            LogUtil.info(pluginName, "auditStatus: " + auditStatus);
+            LogUtil.info(pluginName, "applicationLastAction: " + applicationLastAction);
+            LogUtil.info(pluginName, "Submit Type: " + submitType);
+            
+            String appStatus = aad.getStatus(key, tableProcessName);
+            LogUtil.info(pluginName, appStatus);
+            
+            if(appStatus.equalsIgnoreCase("PENDING LAST APPROVAL")){
+                this.ActivityCompleted(key, submodule, actorFullname, actorUsername, remarkMessage, applicationStatus, auditActivity, auditStatus, applicationLastAction, tableName, submitType);
+                result.setType(result.TYPE_REDIRECT);
+                result.setMessage(remarkMessage);
+            }else{
+                result.setType(result.TYPE_ERROR);
+                result.setMessage("Check application status or reload the page!");
+            }
+        }
+        
+        return result;
+    }
+    
+    public void ActivityCompleted(String key, String submodule, String actorFullname, String actorUsername, String remarkMessage, String applicationStatus, String auditActivity, String auditStatus, String applicationLastAction, String tableName, String submitType) {
+        WorkflowManager workflowManager = (WorkflowManager) AppUtil.getApplicationContext().getBean("workflowManager");
+
+        Map<String, String> activityData = new HashMap();
+        activityData = aad.getActivityData(key);
+
+        workflowManager.activityVariable(activityData.get("activity_id"), "status", submitType);
+        workflowManager.assignmentForceComplete(activityData.get("process_def_id"), activityData.get("process_id"), activityData.get("activity_id"), actorUsername);
+
+        if (submodule.equalsIgnoreCase("levy exemption")) {
+            if (aad.updateStatusLevyExemption(key, applicationStatus, actorFullname, actorUsername, applicationLastAction)) {
+                LogUtil.info("action button lex", "st: true");
+                aad.insertAuditTrailLevyExemption(actorFullname, actorUsername, auditActivity, key, auditStatus, remarkMessage);
+            }
+        } else if (submodule.equalsIgnoreCase("levy exemption criteria")) {
+            if (aad.updateStatusLevyExemptionCriteria(key, applicationStatus, actorFullname, actorUsername, applicationLastAction)) {
+                LogUtil.info("action button lexc", "st: true");
+                aad.insertAuditTrailLevyExemptionCriteria(actorFullname, actorUsername, auditActivity, key, auditStatus, remarkMessage);
+            }
+        } else if (submodule.equalsIgnoreCase("interest waiver")) {
+            if (aad.updateStatusInterestWaiver(key, applicationStatus, actorFullname, actorUsername, applicationLastAction)) {
+                LogUtil.info("action button inw", "st: true");
+                aad.insertAuditTrailWaiver(actorFullname, actorUsername, auditActivity, key, auditStatus, remarkMessage, tableName);
+            }
+        } 
+    }
+
+    @Override
+    public String getLabel() {
+        return pluginName;
+    }
+
+    @Override
+    public String getClassName() {
+        return this.getClass().getName();
+    }
+
+    @Override
+    public String getPropertyOptions() {
+        String formDefField = null;
+        AppDefinition appDef = AppUtil.getCurrentAppDefinition();
+        if (appDef != null) {
+            String formJsonUrl = "[CONTEXT_PATH]/web/json/console/app/" + appDef.getId() + "/" + appDef.getVersion() + "/forms/options";
+            formDefField = "{name:'formDefId',label:'@@form.defaultformoptionbinder.formId@@',type:'selectbox',required:'True',options_ajax:'" + formJsonUrl + "'}";
+        } else {
+            formDefField = "{name:'formDefId',label:'@@form.defaultformoptionbinder.formId@@',type:'textfield',required:'True'}";
+        }
+        Object[] arguments = new Object[]{formDefField};
+        String json = AppUtil.readPluginResource(getClass().getName(), "/properties/approverActionSettings.json", arguments, true,null);
+        return json;
+//        return AppUtil.readPluginResource(getClassName(), "/properties/approverActionSettings.json", null, true, null);
+    }
+    
+}
---
+++ src/main/java/com/test/plugin/dao/ApproverActionDao.java
@@ -0,0 +1,450 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package id.co.itasoft.hrdc.plugin.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
+import javax.sql.DataSource;
+import org.joget.apps.app.service.AppUtil;
+import org.joget.commons.util.LogUtil;
+
+/**
+ *
+ * @author WEEBS
+ */
+public class ApproverActionDao {
+    public boolean updateStatusLevyExemption(String recordId, String status, String name, String username, String action) {
+        DataSource ds = (DataSource) AppUtil.getApplicationContext().getBean("setupDataSource");
+        Connection con = null;
+        PreparedStatement ps = null;
+        StringBuilder query = new StringBuilder();
+
+        boolean result = false;
+
+        query
+                .append(" UPDATE")
+                .append(" app_fd_levm_lex_app")
+                .append(" SET")
+                .append(" c_status=?,")
+                .append(" c_last_action=?,")
+                .append(" dateModified=CURRENT_TIMESTAMP(),")
+                .append(" modifiedBy=?,")
+                .append(" modifiedByName=?")
+                .append(" WHERE")
+                .append(" id=?");
+
+        try {
+            con = ds.getConnection();
+            ps = con.prepareStatement(query.toString());
+            ps.setString(1, status);
+            ps.setString(2, action);
+            ps.setString(3, username);
+            ps.setString(4, name);
+            ps.setString(5, recordId);
+
+            int i = ps.executeUpdate();
+            if (i > 0) {
+                result = true;
+            }
+        } catch (SQLException e) {
+            LogUtil.error(this.getClass().getName(), e, "Error : " + e.getMessage());
+        } finally {
+            if (ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException ex) {
+                    LogUtil.error(this.getClass().getName(), ex, "Error : " + ex.getMessage());
+                }
+            }
+            if (con != null) {
+                try {
+                    con.close();
+                } catch (SQLException ex) {
+                    LogUtil.error(this.getClass().getName(), ex, "Error : " + ex.getMessage());
+                }
+            }
+        }
+        return result;
+    }
+    
+    public boolean insertAuditTrailLevyExemption(String name, String username, String activityName, String parentId, String status, String remark) {
+        DataSource ds = (DataSource) AppUtil.getApplicationContext().getBean("setupDataSource");
+
+        PreparedStatement ps = null;
+        Connection con = null;
+        StringBuilder query = new StringBuilder();
+        int insertReturn;
+        boolean result = false;
+
+        query
+                .append("INSERT INTO app_fd_levm_lex_app_audit (id, dateCreated, dateModified, createdBy, createdByName, modifiedBy, modifiedByName, c_activity_name, c_approval_date, c_parent_id, c_name, c_username, c_status, c_remark)")
+                .append("VALUES (UUID(), CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP(), ?, ?, ?, ?, ?, DATE_FORMAT(CURRENT_TIMESTAMP(), '%b %d, %Y %r'), ?, ?, ?, ?, ?)");
+        try {
+            con = ds.getConnection();
+            ps = con.prepareStatement(query.toString());
+            ps.setString(1, username);
+            ps.setString(2, name);
+            ps.setString(3, username);
+            ps.setString(4, name);
+            ps.setString(5, activityName);
+            ps.setString(6, parentId);
+            ps.setString(7, name);
+            ps.setString(8, username);
+            ps.setString(9, status);
+            ps.setString(10, remark);
+            LogUtil.info("action button", ps.toString());
+            insertReturn = ps.executeUpdate();
+
+            if (insertReturn > 0) {
+                result = true;
+            }
+        } catch (SQLException e) {
+            LogUtil.error(this.getClass().getName(), e, "Error : " + e.getMessage());
+        } finally {
+            if (ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException ex) {
+                    LogUtil.error(this.getClass().getName(), ex, "Error : " + ex.getMessage());
+                }
+            }
+            if (con != null) {
+                try {
+                    con.close();
+                } catch (SQLException ex) {
+                    LogUtil.error(this.getClass().getName(), ex, "Error : " + ex.getMessage());
+                }
+            }
+        }
+        return result;
+    }
+    
+    public boolean updateStatusLevyExemptionCriteria(String recordId, String status, String name, String username, String action) {
+        DataSource ds = (DataSource) AppUtil.getApplicationContext().getBean("setupDataSource");
+        Connection con = null;
+        PreparedStatement ps = null;
+        StringBuilder query = new StringBuilder();
+
+        boolean result = false;
+
+        query
+                .append(" UPDATE")
+                .append(" app_fd_levm_lex_criteria")
+                .append(" SET")
+                .append(" c_status=?,")
+                .append(" c_last_action=?,")
+                .append(" dateModified=CURRENT_TIMESTAMP(),")
+                .append(" modifiedBy=?,")
+                .append(" modifiedByName=?")
+                .append(" WHERE")
+                .append(" id=?");
+
+        try {
+            con = ds.getConnection();
+            ps = con.prepareStatement(query.toString());
+            ps.setString(1, status);
+            ps.setString(2, action);
+            ps.setString(3, username);
+            ps.setString(4, name);
+            ps.setString(5, recordId);
+
+            int i = ps.executeUpdate();
+            if (i > 0) {
+                result = true;
+            }
+        } catch (SQLException e) {
+            LogUtil.error(this.getClass().getName(), e, "Error : " + e.getMessage());
+        } finally {
+            if (ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException ex) {
+                    LogUtil.error(this.getClass().getName(), ex, "Error : " + ex.getMessage());
+                }
+            }
+            if (con != null) {
+                try {
+                    con.close();
+                } catch (SQLException ex) {
+                    LogUtil.error(this.getClass().getName(), ex, "Error : " + ex.getMessage());
+                }
+            }
+        }
+
+        return result;
+
+    }
+
+    public boolean insertAuditTrailLevyExemptionCriteria(String name, String username, String activityName, String parentId, String status, String remark) {
+        DataSource ds = (DataSource) AppUtil.getApplicationContext().getBean("setupDataSource");
+
+        PreparedStatement ps = null;
+        Connection con = null;
+        StringBuilder query = new StringBuilder();
+        int insertReturn;
+        boolean result = false;
+
+        query
+                .append("INSERT INTO app_fd_levm_lex_crtr_audit (id, dateCreated, dateModified, createdBy, createdByName, modifiedBy, modifiedByName, c_activity_name, c_approval_date, c_parent_id, c_name, c_username, c_status, c_remark)")
+                .append("VALUES (UUID(), CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP(), ?, ?, ?, ?, ?, DATE_FORMAT(CURRENT_TIMESTAMP(), '%b %d, %Y %r'), ?, ?, ?, ?, ?)");
+        try {
+            con = ds.getConnection();
+            ps = con.prepareStatement(query.toString());
+            ps.setString(1, username);
+            ps.setString(2, name);
+            ps.setString(3, username);
+            ps.setString(4, name);
+            ps.setString(5, activityName);
+            ps.setString(6, parentId);
+            ps.setString(7, name);
+            ps.setString(8, username);
+            ps.setString(9, status);
+            ps.setString(10, remark);
+            LogUtil.info("action button", ps.toString());
+            insertReturn = ps.executeUpdate();
+
+            if (insertReturn > 0) {
+                result = true;
+            }
+        } catch (SQLException e) {
+            LogUtil.error(this.getClass().getName(), e, "Error : " + e.getMessage());
+        } finally {
+            if (ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException ex) {
+                    LogUtil.error(this.getClass().getName(), ex, "Error : " + ex.getMessage());
+                }
+            }
+            if (con != null) {
+                try {
+                    con.close();
+                } catch (SQLException ex) {
+                    LogUtil.error(this.getClass().getName(), ex, "Error : " + ex.getMessage());
+                }
+            }
+        }
+        return result;
+    }
+
+    public boolean updateStatusInterestWaiver(String recordId, String status, String name, String username, String action) {
+        DataSource ds = (DataSource) AppUtil.getApplicationContext().getBean("setupDataSource");
+        Connection con = null;
+        PreparedStatement ps = null;
+        StringBuilder query = new StringBuilder();
+
+        boolean result = false;
+
+        query
+                .append(" UPDATE")
+                .append(" app_fd_levm_inw_app")
+                .append(" SET")
+                .append(" c_status=?,")
+                .append(" c_last_action=?,")
+                .append(" dateModified=CURRENT_TIMESTAMP(),")
+                .append(" modifiedBy=?,")
+                .append(" modifiedByName=?")
+                .append(" WHERE")
+                .append(" id=?");
+
+        try {
+            con = ds.getConnection();
+            ps = con.prepareStatement(query.toString());
+            ps.setString(1, status);
+            ps.setString(2, action);
+            ps.setString(3, username);
+            ps.setString(4, name);
+            ps.setString(5, recordId);
+
+            int i = ps.executeUpdate();
+            if (i > 0) {
+                result = true;
+            }
+        } catch (SQLException e) {
+            LogUtil.error(this.getClass().getName(), e, "Error : " + e.getMessage());
+        } finally {
+            if (ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException ex) {
+                    LogUtil.error(this.getClass().getName(), ex, "Error : " + ex.getMessage());
+                }
+            }
+            if (con != null) {
+                try {
+                    con.close();
+                } catch (SQLException ex) {
+                    LogUtil.error(this.getClass().getName(), ex, "Error : " + ex.getMessage());
+                }
+            }
+        }
+        return result;
+    }
+    
+    public boolean insertAuditTrailWaiver(String name, String username, String activityName, String parentId, String status, String remark, String tableName) {
+        DataSource ds = (DataSource) AppUtil.getApplicationContext().getBean("setupDataSource");
+
+        PreparedStatement ps = null;
+        Connection con = null;
+        StringBuilder query = new StringBuilder();
+        int insertReturn;
+        boolean result = false;
+
+        query
+                .append("INSERT INTO app_fd_"+tableName+" (id, dateCreated, dateModified, createdBy, createdByName, modifiedBy, modifiedByName, c_activity_name, c_approval_date, c_parent_id, c_name, c_username, c_status, c_remark)")
+                .append("VALUES (UUID(), CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP(), ?, ?, ?, ?, ?, DATE_FORMAT(CURRENT_TIMESTAMP(), '%b %d, %Y %r'), ?, ?, ?, ?, ?)");
+        try {
+            con = ds.getConnection();
+            ps = con.prepareStatement(query.toString());
+            ps.setString(1, username);
+            ps.setString(2, name);
+            ps.setString(3, username);
+            ps.setString(4, name);
+            ps.setString(5, activityName);
+            ps.setString(6, parentId);
+            ps.setString(7, name);
+            ps.setString(8, username);
+            ps.setString(9, status);
+            ps.setString(10, remark);
+            LogUtil.info("action button", ps.toString());
+            insertReturn = ps.executeUpdate();
+
+            if (insertReturn > 0) {
+                result = true;
+            }
+        } catch (SQLException e) {
+            LogUtil.error(this.getClass().getName(), e, "Error : " + e.getMessage());
+        } finally {
+            if (ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException ex) {
+                    LogUtil.error(this.getClass().getName(), ex, "Error : " + ex.getMessage());
+                }
+            }
+            if (con != null) {
+                try {
+                    con.close();
+                } catch (SQLException ex) {
+                    LogUtil.error(this.getClass().getName(), ex, "Error : " + ex.getMessage());
+                }
+            }
+        }
+        return result;
+    }
+    
+    public String getStatus(String id, String tableProcessName) {
+        DataSource ds = (DataSource) AppUtil.getApplicationContext().getBean("setupDataSource");
+
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        Connection con = null;
+        StringBuilder query = new StringBuilder();
+        String result = "";
+
+        query.append("SELECT c_status FROM app_fd_"+tableProcessName+" WHERE id = ? LIMIT 1");
+        try {
+            con = ds.getConnection();
+            ps = con.prepareStatement(query.toString());
+            ps.setString(1, id);
+            rs = ps.executeQuery();
+
+            while (rs.next()) {
+                result = rs.getString(1);
+            }
+        } catch (SQLException e) {
+            LogUtil.error(this.getClass().getName(), e, "Error : " + e.getMessage());
+        } finally {
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException ex) {
+                    LogUtil.error(this.getClass().getName(), ex, "Error : " + ex.getMessage());
+                }
+            }
+            if (ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException ex) {
+                    LogUtil.error(this.getClass().getName(), ex, "Error : " + ex.getMessage());
+                }
+            }
+            if (con != null) {
+                try {
+                    con.close();
+                } catch (SQLException ex) {
+                    LogUtil.error(this.getClass().getName(), ex, "Error : " + ex.getMessage());
+                }
+            }
+        }
+        return result;
+    }
+    
+    public HashMap<String, String> getActivityData(String processRecordId) {
+
+        DataSource ds = (DataSource) AppUtil.getApplicationContext().getBean("setupDataSource");
+
+        PreparedStatement ps = null;
+        Connection con = null;
+        StringBuilder query = new StringBuilder();
+        ResultSet rs = null;
+
+        HashMap<String, String> activityData = new HashMap();
+
+        query
+                .append("SELECT")
+                .append(" s.Id as activity_id,")
+                .append(" s.ProcessId as process_id,")
+                .append(" s.PDefName as process_def_id,")
+                .append(" s.Name as activity_name")
+                .append(" FROM")
+                .append(" wf_process_link wpl LEFT JOIN SHKActivities s")
+                .append(" ON wpl.processId=s.ProcessId")
+                .append(" WHERE")
+                .append(" wpl.parentProcessId=?")
+                .append(" AND (s.State='1000003' OR s.State='1000001')");
+        try {
+            con = ds.getConnection();
+            ps = con.prepareStatement(query.toString());
+            ps.setString(1, processRecordId);
+            rs = ps.executeQuery();
+
+            while (rs.next()) {
+                activityData.put("activity_id", rs.getString("activity_id"));
+                activityData.put("process_id", rs.getString("process_id"));
+                activityData.put("process_def_id", rs.getString("process_def_id"));
+                activityData.put("activity_name", rs.getString("activity_name"));
+            }
+        } catch (SQLException e) {
+            LogUtil.error(this.getClass().getName(), e, "Error : " + e.getMessage());
+        } finally {
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException ex) {
+                    LogUtil.error(this.getClass().getName(), ex, "Error : " + ex.getMessage());
+                }
+            }
+            if (ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException ex) {
+                    LogUtil.error(this.getClass().getName(), ex, "Error : " + ex.getMessage());
+                }
+            }
+            if (con != null) {
+                try {
+                    con.close();
+                } catch (SQLException ex) {
+                    LogUtil.error(this.getClass().getName(), ex, "Error : " + ex.getMessage());
+                }
+            }
+        }
+        return activityData;
+    }
+}
