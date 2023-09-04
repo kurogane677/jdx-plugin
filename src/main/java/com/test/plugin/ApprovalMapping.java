01a150288da953b9f2d9fb3b69ef640c6e46323e
kurogane677 <85788933+kurogane677@users.noreply.github.com>
2023-09-04 00:17:39 -0700

    Update ApprovalMapping.java

--- src/main/java/com/test/plugin/ApprovalMapping.java
+++ src/main/java/com/test/plugin/ApprovalMapping.java
@@ -2,9 +2,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package id.co.itasoft.hrdc.plugin;
+package com.test.plugin;
 
-import id.co.itasoft.hrdc.plugin.dao.ApprovalMappingDao;
+import com.test.plugin.dao.ApprovalMappingDao;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
